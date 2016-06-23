3c15dd3e21355676cabb2b0210105279c680fbd9
Clayton Peirens (Contractor) <Clayton.Peirens@sjrb.ca>
2016-06-23 08:41:35 -0600

    Make the puppet.conf update task more readable and less prone to merge conflicts

--- files/migrate_to_new_master.ps1
+++ files/migrate_to_new_master.ps1
@@ -5,7 +5,10 @@ $ssldir = $confdir + "/ssl"
 
 # update the server and archive_file_server settings with the new puppet master fqdn
 # remove the enviroment setting
-(((Get-Content ($confdir + "/puppet.conf") | Foreach-Object {$_ -replace '^server.+', ("server=" + $to_fqdn)}) | Foreach-Object {$_ -replace '^archive_file_server.+', ("archive_file_server=" + $to_fqdn)}) | Where-Object {$_ -notmatch '^environment.+'}) | Set-Content ($confdir + "/puppet.conf")
+(((Get-Content ($confdir + "/puppet.conf") |
+   Foreach-Object {$_ -replace '^server.+', ("server=" + $to_fqdn)}) |
+   Foreach-Object {$_ -replace '^archive_file_server.+', ("archive_file_server=" + $to_fqdn)}) |
+   Where-Object {$_ -notmatch '^environment.+'}) | Set-Content ($confdir + "/puppet.conf")
 
 # remove the ssl directory and all contents
 Remove-Item -Recurse -Force $ssldir
